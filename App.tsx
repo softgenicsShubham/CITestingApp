2b1e8a68b57cc3f1dd51680cd80ebed2f2fd7651
Shubham ghanghotia <Softgenics.shubham@gmail.com>
2024-08-26 16:30:19 +0530

    fixed lint

--- App.tsx
+++ App.tsx
@@ -1,14 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
-const App = () => {
+const App: React.FC = () => {
   return (
-    <View>
+    <View style={styles.main_container}>
       <Text>App</Text>
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  main_container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
